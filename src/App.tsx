1bb04c2de9f94686fee97857eb08bfc09869bfb8
niiquash <qua16006@byui.edu>
2023-07-14 14:53:46 -0600

    used axios to fetch data from endpoint and handled error

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios, { CanceledError } from "axios";
+
+interface Comment {
+  id: number;
+  name: string;
+  body: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get<Comment[]>("https://jsonplaceholder.typicode.com/comments", {
+        signal: controller.signal,
+      })
+      .then((res) => setComments(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {error && <p className="text-danger">{error}</p>}
+      <ul className="list-group">
+        {comments.map((comment) => (
+          <li className="list-group-item" key={comment.id}>
+            <p>Name: {comment.name}</p>
+            <p>Body: {comment.body}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
